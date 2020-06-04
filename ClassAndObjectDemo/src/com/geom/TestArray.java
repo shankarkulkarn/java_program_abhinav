f1592ead4ceab0c776518ec862e57fa9c4ee4d34
shankar <shankarkulkarn@gmail.com>
2020-06-04 17:41:12 +0530

    class added

---
+++ ClassAndObjectDemo/src/com/geom/TestArray.java
@@ -0,0 +1,28 @@
+package com.geom;
+
+import java.util.Random;
+
+//  Array of object 
+// statement tightly coupled , loosely coupled 
+public class TestArray {
+
+	public static void main(String[] args) {
+		
+		Rectangle  [] rectangle = new Rectangle[5];
+		
+		for(int i=0; i<rectangle.length  ;i++)
+		{
+		rectangle[i]= new Rectangle();
+		}
+		
+		Random  random = new Random();
+		for(int i=0;i<rectangle.length ;i++)
+		{
+		int length = random.nextInt(100);
+		int width = random.nextInt(100);
+	    rectangle[i].set(length,width);
+		System.out.println(rectangle[i].area());
+		}
+	}
+
+}
